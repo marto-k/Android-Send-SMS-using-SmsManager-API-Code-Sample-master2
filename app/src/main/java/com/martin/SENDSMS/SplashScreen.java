4ace6cbdc7cf32764b2d5371b7bd0cc266097e22
marto-k <martow17@gmail.com>
2016-05-05 13:01:37 +0300

    add A

---
+++ app/src/main/java/com/martin/SENDSMS/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.martin.SENDSMS;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+public class SplashScreen extends SendSMSActivity {
+
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splash_screen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this,SendSMSActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
